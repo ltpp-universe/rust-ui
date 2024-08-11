292e39bd2cc0009afb77de97ddba48cad1e33032
尤雨东 <root@ltpp.vip>
2024-08-11 23:22:25 +0800

    feat:update

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/rust-ui.git;
+git remote add github git@github.com:ltpp-universe/rust-ui.git;
+git remote add origin git@github.com:ltpp-universe/rust-ui.git;
